31e8dfe76bcc97c314aabc511f02698d49201a5d
mgarrido <m.garridoj.2018@alumnos.urjc.es>
2023-02-20 12:04:11 +0100

    Abriendo la pagina web visualmente

--- P1/tienda.js
+++ P1/tienda.js
@@ -23,16 +23,37 @@ const server = http.createServer((req, res) => {
     // Creamos una variable vacia para almacenar las peticiones
     let recurso = "";
     if(myURL.pathname == '/'){
-      recurso += pagina
+      recurso += pagina;
+      console.log(recurso);
+
     }
-      else {
-        code = 404;
-        code_msg = "Not found"
-        page =  pag_error;
-    };
-
-
+    else{
+      recurso += myURL.pathname.substring(1);
+      
+    }
+    fs.stat(recurso, error => {    
+      if (!error) {    
+          //-- Lectura asíncrona
+          fs.readFile(recurso, (error, page) => {
+              //-- Petición 200 OK
+              res.writeHead(200, {'Content-Type': mime});
+              console.log("Petición 200 OK");
+              res.write(page);
+              res.end();
+          });
   
+      }else{
+          //-- 404 ERROR
+          console.log("404 ERROR");
+          //-- Lectura asíncrona
+          fs.readFile(PAG_ERR,(error,page) => {
+              res.writeHead(404, {'Content-Type': mime});
+              res.write(page);
+              res.end();    
+          });
+      }; 
+  });
+  });
 
    //Definicion de todos los tipos de archivo
    const mime = {
@@ -44,6 +65,7 @@ const server = http.createServer((req, res) => {
     "ico" : "image/ico",
     "css" : "text/css",
   };
+
   //El servidor escucha, y pasamos una linea que nos lo muestre y ademas nos diga el puerto.
   server.listen(PUERTO);
   console.log("Servidor activado. Escuchando en puerto " + PUERTO);
