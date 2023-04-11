e54d3324601094525ba3ee9e57d5cddfaf8b28c2
mgarrido <m.garridoj.2018@alumnos.urjc.es>
2023-04-11 14:16:07 +0200

    Intento inicio sesion

--- P2/tienda.js
+++ P2/tienda.js
@@ -9,11 +9,13 @@ const PUERTO = 9000;
 const pagina = 'tienda.html';
 const pag_error = 'error.html';
 const favicon = 'favicon-16x16.png';
+const USUARIOS = {
+  "Marta6099" : "12345marta"
+};
+
 //-- Cargar pagina web del formulario
 const FORMULARIO = fs.readFileSync('inicio.html','utf-8');
 
-//-- HTML de la página de respuesta
-const RESPUESTA = fs.readFileSync('usuario.html', 'utf-8');
 
 // Definición de tipos MIME
 const mime = {
@@ -81,20 +83,58 @@ const server = http.createServer((req, res) => {
       }); 
     }
   });
-   // Entregar el formulario
-   let content = FORMULARIO;
-   //-- Leer los parámetros
-  let correo = myURL.searchParams.get('Correo');
-  let contraseña = myURL.searchParams.get('Contraseña');
-  console.log(" Correo: " + correo);
-  console.log(" Contraseña: " + contraseña);
+ 
+  // Registramos usuarios
+  if (myURL.pathname == '/registrar') {
+    let contenido = '';
+  //Leer los datos del formulario
+  req.on('data', data => {
+    contenido += data;
+  });
+  // Procesar los datos del formulario
+req.on('end', () => {
+  const datos = new URLSearchParams(contenido);
+  const nombre = datos.get('nombre');
+  const apellidos = datos.get('apellidos');
+  const correo = datos.get('correo');
+  const contrasena = datos.get('contrasena');
 
-  //-- si el usuario es Chuck Norris se añade HTML extra
-  let html_extra = "";
-  if (nombre=="Marta6099" && apellidos=="12345marta") {
-     html_extra = "<h2>Marta6099 no necesita registrarse</h2>";
+  // Verificar si el usuario ya está registrado
+  if (USUARIOS.some(u => u.correo === correo)) {
+    res.writeHead(409, { 'Content-Type': 'text/plain' });
+    res.end('El usuario ya está registrado');
+    return;
   }
-  content = content.replace("HTML_EXTRA", html_extra);
+
+  // Agregar el nuevo usuario
+  usuarios.push({ nombre, apellidos, correo, contrasena });
+  console.log('Usuario registrado:', { nombre, apellidos, correo, contrasena });
+
+  // Redirigir a la página de inicio de sesión
+  res.writeHead(302, { 'Location': '/inicio-sesion' });
+  res.end();
+});
+
+return;
+  }
+// Inicio de sesión
+if (myURL.pathname == '/iniciar-sesion') {
+  let contenido = '';
+// Leer los datos del formulario
+req.on('data', data => {
+  contenido += data;
+});
+
+// Procesar los datos del formulario
+req.on('end', () => {
+  const datos = new URLSearchParams(contenido);
+  const correo = datos.get('correo');
+  const contrasena = datos.get('contrasena');
+
+  // Verificar si el usuario existe y las credenciales son correctas
+  const usuario = usuarios.find(u => u.correo === correo && u.contrasena === contrasena);
+
+ 
 });
 
 // El servidor escucha, y pasamos una línea que nos lo muestre y además nos diga el puerto.
