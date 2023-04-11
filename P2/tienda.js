b22dfabebd8ce8429209edc7894c317a6850fa26
mgarrido <m.garridoj.2018@alumnos.urjc.es>
2023-04-11 14:04:56 +0200

    INtento usuarios

--- P2/tienda.js
+++ P2/tienda.js
@@ -9,6 +9,11 @@ const PUERTO = 9000;
 const pagina = 'tienda.html';
 const pag_error = 'error.html';
 const favicon = 'favicon-16x16.png';
+//-- Cargar pagina web del formulario
+const FORMULARIO = fs.readFileSync('inicio.html','utf-8');
+
+//-- HTML de la página de respuesta
+const RESPUESTA = fs.readFileSync('usuario.html', 'utf-8');
 
 // Definición de tipos MIME
 const mime = {
@@ -76,6 +81,20 @@ const server = http.createServer((req, res) => {
       }); 
     }
   });
+   // Entregar el formulario
+   let content = FORMULARIO;
+   //-- Leer los parámetros
+  let correo = myURL.searchParams.get('Correo');
+  let contraseña = myURL.searchParams.get('Contraseña');
+  console.log(" Correo: " + correo);
+  console.log(" Contraseña: " + contraseña);
+
+  //-- si el usuario es Chuck Norris se añade HTML extra
+  let html_extra = "";
+  if (nombre=="Marta6099" && apellidos=="12345marta") {
+     html_extra = "<h2>Marta6099 no necesita registrarse</h2>";
+  }
+  content = content.replace("HTML_EXTRA", html_extra);
 });
 
 // El servidor escucha, y pasamos una línea que nos lo muestre y además nos diga el puerto.
