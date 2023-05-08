3445e44aec06eb341a543ab72d7f717c248afe90
mgarrido <m.garridoj.2018@alumnos.urjc.es>
2023-05-08 12:23:37 +0200

    Actualizando electron

--- P4/main.js
+++ P4/main.js
@@ -18,9 +18,27 @@ electron.app.on('ready', () => {
     // Creamos la ventana principal de nuestra aplicación
     win = new electron.BrowserWindow({
         width: 600,  //-- Anchura 
-        height: 400  //-- Altura
+        height: 600,  //-- Altura
+
+    //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     });
     // Para quitar el menu
     // win.setMenuBarVisibility(false)
     win.loadFile("menu.html");
-});
+
+    win.on('ready-to-show', () => {
+        console.log("HOLA?");
+        win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+      });
+      win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+
+    });
+
+      electron.ipcMain.handle('test', (event, msg) => {
+        console.log("-> Mensaje: " + msg);
+      });
+
